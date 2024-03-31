50adbeb838bd32a6cc157ad59e2364eb4b7020c4
Julio Malfa <sba23065@student.cct.ie>
2024-03-31 14:06:29 +0100

    DBConnector
    Created java class called DBConnector where is saved the credentials to connect to our DB and the SQL query to display our Course Report.

---
+++ src/ca3/DBConnector.java
@@ -0,0 +1,66 @@
+package ca3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author julio
+ */
+public class DBConnector {
+     // final - constant, cannot change (I Know it is bad practise to have user and password saved like this
+    private final String DB_URL = "jdbc:mysql://localhost";
+    private final String USER = "pooa2024";
+    private final String PASSWORD = "pooa2024";
+    
+    void displayCourseReport() throws SQLException {
+        // Connecting to Database 
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        Statement stmt = conn.createStatement();
+        stmt.execute("USE ca3;");
+
+        // Executing SQL query
+        ResultSet rs = stmt.executeQuery("SELECT \n" +
+                "    m.module_name,\n" +
+                "    c.program AS program,\n" +
+                "    COUNT(e.student_id) AS num_students_enrolled,\n" +
+                "    m.lecturer_name,\n" +
+                "    COALESCE(m.room_name, 'Online') AS room_name\n" +
+                "FROM \n" +
+                "    modules m\n" +
+                "JOIN courses c ON m.course_name = c.course_name\n" +
+                "LEFT JOIN enrollments e ON m.module_id = e.module_id\n" +
+                "GROUP BY \n" +
+                "    m.module_id;");
+
+        // Printing out report header
+        System.out.println("Module Name                            | Program                    | Number of Students Enrolled   | Lecturer Name             | Room");
+
+        // Printing out report
+        while (rs.next()) {
+            // Getting the variables from Database
+            String moduleName = rs.getString("module_name");
+            String program = rs.getString("program");
+            int numStudentsEnrolled = rs.getInt("num_students_enrolled");
+            String lecturerName = rs.getString("lecturer_name");
+            String roomName = rs.getString("room_name");
+
+            // Adjust column widths based on the maximum length of data in each column
+            System.out.printf("%-38s| %-28s| %-30d| %-25s| %-10s\n",
+                    moduleName,
+                    program,
+                    numStudentsEnrolled,
+                    lecturerName,
+                    roomName);
+        }
+    }
+    
+}
